5e425e27700c1d8a770f88653d68cfa67a005fd2
jefersongjr <jeferson_gjr@hotmail.com>
2022-05-09 17:38:26 -0300

    requisito 8, renderizando Title dentro de Missions

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
-        <p>Olá</p>
+        <Title headline="Missões" />
       </div>
     );
   }
