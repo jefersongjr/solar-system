e876a11e7741bc3bc34393c92df981aaea3f6fd7
jefersongjr <jeferson_gjr@hotmail.com>
2022-05-09 17:27:52 -0300

    corrigindo requisito 4

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Propstypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetImage, planetName } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{ planetName }</p>
+        <img src={ planetImage } alt={ `planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: Propstypes.string.isRequired,
+  planetImage: Propstypes.string.isRequired,
+};
+
+export default PlanetCard;
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -5,7 +5,7 @@ class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <Title headline="Planeta" />
+        <Title headline="Planetas" />
       </div>
     );
   }
