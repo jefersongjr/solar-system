c57684a653b577c605c78ce1b71b7648e4e89013
jefersongjr <jeferson_gjr@hotmail.com>
2022-05-09 18:22:06 -0300

    corrigindo requisito 10

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <section>
+          {
+            missions.map((mission) => (
+              <MissionCard
+                name={ mission.name }
+                year={ mission.year }
+                country={ mission.country }
+                destination={ mission.destination }
+                key={ mission.name }
+              />
+            ))
+          }
+        </section>
       </div>
     );
   }
