982383d338e189278088fd58cac5cebf960670f2
jefersongjr <jeferson_gjr@hotmail.com>
2022-05-10 13:37:23 -0300

    estilizando o projeto

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -8,7 +8,7 @@ class Missions extends React.Component {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
-        <section>
+        <section className="mission-section">
           {
             missions.map((mission) => (
               <MissionCard
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -8,7 +8,7 @@ class SolarSystem extends React.Component {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
-        <section>
+        <section className="planet-section">
           {
             planets.map((planet) => (
               <PlanetCard
