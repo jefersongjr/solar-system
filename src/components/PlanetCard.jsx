55c365559a446857307e0cbd2990827f110c067c
jefersongjr <jeferson_gjr@hotmail.com>
2022-11-10 15:01:49 -0300

    fix : corrigindo lint

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -6,8 +6,12 @@ class PlanetCard extends React.Component {
     const { planetImage, planetName } = this.props;
     return (
       <div data-testid="planet-card">
-        <p data-testid="planet-name">{ planetName }</p>
-        <img src={ planetImage } className={`image${planetName}`} alt={ `Planeta ${planetName}` } />
+        <p data-testid="planet-name">{planetName}</p>
+        <img
+          src={ planetImage }
+          className={ `image${planetName}` }
+          alt={ `Planeta ${planetName}` }
+        />
       </div>
     );
   }
